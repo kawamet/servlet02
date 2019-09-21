b06462f5390dd8ee55a84ce11e5351aaaac9dca5
Karolina Wanat <carolinawanat@gmail.com>
2019-09-21 11:15:03 +0100

    working version

--- src/java/ConfigurationServlet.java
+++ src/java/ConfigurationServlet.java
@@ -7,20 +7,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloServlet", urlPatterns = {"/hello", "/hi", "/Hello"},
-initParams = {@WebInitParam(name = "who", value = "World"),
-@WebInitParam(name = "times", value = "5")},
-loadOnStartup = 1)
+@WebServlet(name = "HelloServlet",
+        urlPatterns = {"/hello", "/hi", "/Hello"},
+        initParams = {@WebInitParam(name = "who", value = "World"),
+                @WebInitParam(name = "times", value = "5")},
+        loadOnStartup = 1)
 public class ConfigurationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String who = request.getParameter("who");
         String whoInit = this.getInitParameter("who");
+
+        String times = request.getParameter("times");
+        String timesInit = this.getInitParameter("times");
+
         PrintWriter writer = response.getWriter();
         response.setContentType("text/html");
         writer.println("<h2>Hello " + who + "</h2>");
         writer.println("<h2>" + whoInit + " (form init param)</h2>");
+
+
 
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,21 @@
+<%@page contentType="text/html; chartset=UTF-8" %>
 <html>
+<head>
+    <title>Sda Servlets - zajecia drigie</title>
+
+</head>
 <body>
-<h2>Hello World!</h2>
+<h1>Servlets 02</h1>
+<h3>Servlet 1: Configuaration</h3>
+<a href="/servlet02_war/hi">Default</a>
+<form action="/servlet02_war/hi" method="get">
+    Imie:
+    <input type="text" name="who"><br>
+    <input type="submit" value="Wyslij">
+
+</form>
+<hr>
+
+
 </body>
 </html>
